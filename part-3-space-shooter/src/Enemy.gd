e09aa1cc272533ea6b1b9eeb50106e976179e396
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-07-11 18:10:07 -0500

    added spaceshooter scoring

--- part-3-space-shooter/src/Enemy.gd
+++ part-3-space-shooter/src/Enemy.gd
@@ -5,16 +5,16 @@ const ExplosionEffect = preload("res://src/ExplosionEffect.tscn")
 export(int) var SPEED = 30
 export(int) var ARMOR = 3
 
-signal score_up
-
 func _process(delta):
 	position.x -= SPEED * delta
-
 
 func _on_Enemy_body_entered(body):
 	body.queue_free()
 	ARMOR -= 1
 	if ARMOR <= 0:
+		var main = get_tree().current_scene
+		if main.is_in_group("World"):
+			main.score += 10
 		queue_free()
 
 func _exit_tree():
