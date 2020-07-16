21ef7202d6f8dee9caebe8da755f8166807778c3
Nicholas Surmava <nicholas.surmava@gmail.com>
2020-07-16 15:27:11 -0500

    added bullet hit effect

--- part-3-space-shooter/src/Bullet.gd
+++ part-3-space-shooter/src/Bullet.gd
@@ -1,2 +1,17 @@
 extends RigidBody2D
 
+const HitEffect = preload("res://src/BulletHitEffect.tscn")
+
+onready var bulletSound = $BulletSound
+
+func _ready():
+	bulletSound.play()
+	
+func _exit_tree():
+	create_hit_effect()
+
+func create_hit_effect():
+	var main = get_tree().current_scene
+	var hitEffect = HitEffect.instance()
+	main.add_child(hitEffect)
+	hitEffect.global_position = global_position
